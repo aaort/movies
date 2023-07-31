ad93dbe7111c0b865278d78849b46b2cb8dcd701
aort <artashesano@gmail.com>
2023-07-31 17:38:26 +0400

    Add loading animation for movie posters

--- app/components/MoviePoster.tsx
+++ app/components/MoviePoster.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Image from 'next/image';
+import { useState } from 'react';
 
 type Props = {
   movie: Movie;
@@ -9,6 +10,7 @@ type Props = {
 const imagesBaseUrl = 'https://image.tmdb.org/t/p/original/';
 
 export default function MoviePoster({ movie }: Props) {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   return (
     <div className="flex flex-col gap-10 bg-slate-300 text-slate-900 rounded-md p-4">
       <div className="relative w-full aspect-square overflow-clip rounded-md">
@@ -16,7 +18,12 @@ export default function MoviePoster({ movie }: Props) {
           fill
           alt=""
           src={`${imagesBaseUrl}${movie.poster_path}`}
-          style={{ objectFit: 'fill' }}
+          className={`object-fill duration-700 ease-in-out ${
+            isLoading
+              ? 'scale-110 blur-2xl grayscale'
+              : 'scale-100 blur-0 grayscale-0'
+          }`}
+          onLoadingComplete={() => setIsLoading(false)}
         />
       </div>
       <div>
