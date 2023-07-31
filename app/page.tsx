00efb78df37870e07b8b4211ee2da024bd2de6e2
aort <artashesano@gmail.com>
2023-07-31 16:23:23 +0400

    Display trending movies in main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,32 @@
 'use client';
 
+import getTrendingMovies from '@/lib/getTrendingMovies';
 import { redirect } from 'next/navigation';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import MoviePoster from './components/MoviePoster';
 
 export default function Home() {
+  const [movies, setMovies] = useState<Movie[]>();
+
   useEffect(() => {
     if (!sessionStorage.getItem('session_id')) {
       redirect('/login');
     }
   });
 
+  useEffect(() => {
+    const setTrendingMovies = async () => {
+      const movies = await getTrendingMovies();
+      setMovies(movies.results);
+    };
+    setTrendingMovies();
+  }, []);
+
   return (
-    <main className="flex flex-col items-center justify-center p-10 gap-10">
-      <h1>All configured and done</h1>
+    <main className="grid p-10 gap-16 grid-cols-4">
+      {movies?.map((movie) => (
+        <MoviePoster key={movie.id} movie={movie} />
+      ))}
     </main>
   );
 }
