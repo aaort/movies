b338a32209f1e7607fa6ad0556217e3c38a7e611
aort <artashesano@gmail.com>
2023-07-31 19:02:55 +0400

    Implement basic structure of header

---
+++ app/movies/layout.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+
+export default function Layout({ children }: React.PropsWithChildren) {
+  return (
+    <div className="flex flex-col h-full justify-between">
+      <header className="flex justify-between items-center py-10 px-2 sm:px-4 md:px-8 lg:px-16 xl:px-32">
+        <h1 className="italic text-xl">Movies</h1>
+        <nav className="flex gap-4">
+          <ul className="flex gap-4">
+            <li>
+              <Link href="#" className="navbar-link">
+                TV Series
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className="navbar-link">
+                People
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className="navbar-link">
+                Upcoming
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className="navbar-link">
+                About
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      {children}
+      <footer></footer>
+    </div>
+  );
+}
