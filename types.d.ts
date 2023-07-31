631c136061d1af4589ae9cd5c20b8d082946cb51
aort <artashesano@gmail.com>
2023-07-31 19:28:06 +0400

    Add Person type

--- types.d.ts
+++ types.d.ts
@@ -15,3 +15,16 @@ type Movie = {
   vote_average: number;
   vote_count: number;
 };
+
+type Person = {
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for: Movie[];
+  known_for_department: string;
+  media_type: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: string;
+};
