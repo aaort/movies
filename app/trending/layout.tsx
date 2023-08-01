6b928295601af3c975eeb520ac69feb89e9ed79c
aort <artashesano@gmail.com>
2023-08-01 12:07:04 +0400

    Update font sizes in navbar

--- app/trending/layout.tsx
+++ app/trending/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: React.PropsWithChildren) {
         <nav>
           <ul className="flex gap-2 sm:gap-4 md:gap-8 lg:gap-12 xl:gap-16 items-center">
             <li className="relative inline-block group">
-              <button>Trending</button>
+              <button className="text-xl">Trending</button>
               <ul className="hidden group-hover:flex flex-col z-10 absolute bg-white border border-gray-300 divide-y divide-gray-100 rounded-md">
                 <li className="px-4 py-2">
                   <Link href="/trending/movies" className="navbar-link">
@@ -42,7 +42,7 @@ export default function Layout({ children }: React.PropsWithChildren) {
           </ul>
         </nav>
       </header>
-      <main className="px-2 sm:px-4 md:px-8 lg:px-16 xl:px-32 py-20">
+      <main className="px-2 sm:px-4 md:px-8 lg:px-16 xl:px-32 mb-10">
         {children}
       </main>
       <footer></footer>
