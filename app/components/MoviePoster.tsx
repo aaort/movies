f6b7be7ac80f1e5ccb742736d891bd058b8d8b1e
aort <artashesano@gmail.com>
2023-08-01 11:48:44 +0400

    Make all cards Server Components

--- app/components/MoviePoster.tsx
+++ app/components/MoviePoster.tsx
@@ -1,7 +1,4 @@
-'use client';
-
 import Image from 'next/image';
-import { useState } from 'react';
 
 type Props = {
   movie: Movie;
@@ -10,7 +7,6 @@ type Props = {
 const imagesBaseUrl = 'https://image.tmdb.org/t/p/original/';
 
 export default function MoviePoster({ movie }: Props) {
-  const [isLoading, setIsLoading] = useState<boolean>(true);
   return (
     <div className="flex flex-col gap-4 bg-slate-300 text-slate-900 rounded-md overflow-clip cursor-pointer duration-200 hover:scale-105">
       <div className="relative w-full aspect-[1/1.3]">
@@ -19,12 +15,7 @@ export default function MoviePoster({ movie }: Props) {
           alt=""
           src={`${imagesBaseUrl}${movie.poster_path}`}
           sizes="(min-width: 1280px) calc(25vw - 112px), (min-width: 1040px) calc(25vw - 80px), (min-width: 780px) calc(25vw - 64px), calc(24.13vw - 49px)"
-          className={`object-fill duration-700 ease-in-out ${
-            isLoading
-              ? 'scale-110 blur-2xl grayscale'
-              : 'scale-100 blur-0 grayscale-0'
-          }`}
-          onLoadingComplete={() => setIsLoading(false)}
+          className="object-fill"
         />
       </div>
       <div className="mx-4 mb-4">
