cd427e8c95aa4502ab18baee5376e29a2d796525
aort <artashesano@gmail.com>
2023-08-01 10:14:49 +0400

    Add trending hover background

--- app/trending/layout.tsx
+++ app/trending/layout.tsx
@@ -6,17 +6,27 @@ export default function Layout({ children }: React.PropsWithChildren) {
     <div className="flex flex-col h-full justify-between">
       <header className="flex justify-between items-center py-10 px-2 sm:px-4 md:px-8 lg:px-16 xl:px-32">
         <Breadcrumbs />
-        <nav className="flex gap-4">
-          <ul className="flex gap-4">
-            <li>
-              <Link href="#" className="navbar-link">
-                TV Series
-              </Link>
-            </li>
-            <li>
-              <Link href="/trending/people" className="navbar-link">
-                People
-              </Link>
+        <nav>
+          <ul className="flex gap-2 sm:gap-4 md:gap-8 lg:gap-12 xl:gap-16 items-center">
+            <li className="relative inline-block group">
+              <button>Trending</button>
+              <ul className="hidden group-hover:flex flex-col z-10 absolute bg-white border border-gray-300 divide-y divide-gray-100 rounded-md">
+                <li className="px-4 py-2">
+                  <Link href="/trending/movies" className="navbar-link">
+                    Movies
+                  </Link>
+                </li>
+                <li className="px-4 py-2">
+                  <Link href="#" className="navbar-link whitespace-nowrap	">
+                    TV Series
+                  </Link>
+                </li>
+                <li className="px-4 py-2">
+                  <Link href="/trending/people" className="navbar-link">
+                    People
+                  </Link>
+                </li>
+              </ul>
             </li>
             <li>
               <Link href="#" className="navbar-link">
