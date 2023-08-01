bbfa4ccd28087b50b769e849db4d7af42b502bde
aort <artashesano@gmail.com>
2023-08-01 11:55:15 +0400

    Reduce columns count on smaller screens in main page

--- app/trending/people/page.tsx
+++ app/trending/people/page.tsx
@@ -5,7 +5,7 @@ export default async function TrendingPeople() {
   const people = (await getTrendingPeople()).results;
 
   return (
-    <div className="grid gap-16 grid-cols-4">
+    <div className="grid gap-16 grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
       {people.map((person) => (
         <Person key={person.id} person={person} />
       ))}
