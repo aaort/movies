d6124240cf926f16c93838750afbacb2327d4e76
aort <artashesano@gmail.com>
2023-08-01 19:13:37 +0400

    Set fonts to Roboto Mono

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
-import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Roboto_Mono } from 'next/font/google';
 import Link from 'next/link';
+import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Roboto_Mono({
+  subsets: ['latin'],
+  weight: ['400', '500', '600'],
+});
 
 export const metadata: Metadata = {
   title: 'Movies',
