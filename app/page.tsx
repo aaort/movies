842ba5c467d1ba4291c137b109523239265ffe61
aort <artashesano@gmail.com>
2023-08-01 14:48:19 +0400

    Remove auth flow

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,5 @@
-'use client';
-
 import { redirect } from 'next/navigation';
-import { useEffect } from 'react';
 
 export default function Home() {
-  useEffect(() => {
-    if (!sessionStorage.getItem('session_id')) {
-      return redirect('/login');
-    }
-
-    redirect('/trending/movies');
-  }, []);
-
-  return (
-    <h2 className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-      Loading...
-    </h2>
-  );
+  redirect('/trending/movies');
 }
---
+++ lib/auth/getGuestSessionId.ts
@@ -0,0 +1,14 @@
+const baseApiUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+const readApiKey = process.env.NEXT_PUBLIC_API_READ_ACCESS_KEY;
+
+export default async function getGuestSessionId() {
+  const data = await fetch(`${baseApiUrl}authentication/guest_session/new`, {
+    headers: {
+      accept: 'application/json',
+      'content-type': 'application/json',
+      Authorization: `Bearer ${readApiKey}`,
+    },
+  });
+
+  return (await data.json()).request_token;
+}
