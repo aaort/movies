ff20175628a292470cd8e523c83d481862f53db2
aort <artashesano@gmail.com>
2023-08-01 17:02:48 +0400

    Update hover animation on posters and TV

--- app/components/Person.tsx
+++ app/components/Person.tsx
@@ -8,14 +8,14 @@ const imagesBaseUrl = 'https://image.tmdb.org/t/p/original/';
 
 export default function Person({ person }: Props) {
   return (
-    <div className="flex flex-col gap-4 bg-slate-300 text-slate-900 rounded-md overflow-clip cursor-pointer duration-200 hover:scale-105">
+    <div className="flex group flex-col gap-4 bg-slate-300 text-slate-900 rounded-md overflow-clip cursor-pointer">
       <div className="relative w-full aspect-[1/1.3]">
         <Image
           fill
           alt=""
           src={`${imagesBaseUrl}${person.profile_path}`}
           sizes="(min-width: 1280px) calc(25vw - 112px), (min-width: 1040px) calc(25vw - 80px), (min-width: 780px) calc(25vw - 64px), calc(24.13vw - 49px)"
-          className="object-fill"
+          className="object-fill group-hover:scale-105 duration-200"
         />
       </div>
       <div className="mx-4 mb-4">
