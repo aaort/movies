0fcc73ab794f7d5cf8bad073b81accfbfdeffbee
aort <artashesano@gmail.com>
2023-08-03 01:14:05 +0400

    Improve flexibility for all pages under /trending

--- app/trending/people/page.tsx
+++ app/trending/people/page.tsx
@@ -5,7 +5,7 @@ export default async function TrendingPeople() {
   const people = (await getTrendingPeople()).results;
 
   return (
-    <div className="grid gap-16 grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+    <div className="grid gap-4 md:gap-10 lg:gap-14 xl:gap-16 grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
       {people.map((person) => (
         <PersonItem key={person.id} person={person} />
       ))}
--- app/trending/tv/page.tsx
+++ app/trending/tv/page.tsx
@@ -5,7 +5,7 @@ export default async function TrendingTV() {
   const tvs = (await getTrendingTV()).results;
 
   return (
-    <div className="grid gap-16 grid-cols-4">
+    <div className="grid gap-4 md:gap-10 lg:gap-14 xl:gap-16 grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
       {tvs.map((tv) => (
         <TVItem key={tv.id} tv={tv} />
       ))}
