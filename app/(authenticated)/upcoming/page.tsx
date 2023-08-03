ed66907178e0c70e7b7a20ec740599363eb36f09
aort <artashesano@gmail.com>
2023-08-03 04:13:54 +0400

    Hide header and footer for auth flow and small bug fix

--- app/(authenticated)/upcoming/page.tsx
+++ app/(authenticated)/upcoming/page.tsx
@@ -1,13 +1,13 @@
 import getUpcomingMovies from '@/lib/getUpcomingMovies';
-import MovieItem from '../components/MovieItem';
+import MovieItem from '@/app/components/MovieItem';
 
 export default async function UpcomingMovies() {
   const movies = (await getUpcomingMovies()).results;
 
   return (
     <div className="grid gap-16 grid-cols-4 ">
-      {movies?.map((movie) => (
-        <MovieItem key={movie.id} movie={movie} />
+      {movies?.map((movie, index) => (
+        <MovieItem key={movie.id} movie={movie} index={index} />
       ))}
     </div>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -21,51 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="flex flex-col h-full justify-between">
-          <header className="flex flex-wrap justify-end gap-y-4 items-center py-10 px-2 sm:px-4 md:px-8 lg:px-16 xl:px-32">
-            <nav>
-              <ul className="flex gap-2 sm:gap-4 md:gap-8 lg:gap-12 xl:gap-16 items-center">
-                <li className="relative inline-block group">
-                  <button className="text-xl">Trending</button>
-                  <ul className="hidden group-hover:flex flex-col z-10 absolute bg-white border border-gray-300 rounded-md shadow-2xl">
-                    <li className="px-4 py-2">
-                      <Link href="/trending/movies" className="navbar-link">
-                        Movies
-                      </Link>
-                    </li>
-                    <li className="px-4 py-2">
-                      <Link
-                        href="/trending/tv"
-                        className="navbar-link whitespace-nowrap w-full"
-                      >
-                        TV Series
-                      </Link>
-                    </li>
-                    <li className="px-4 py-2">
-                      <Link href="/trending/people" className="navbar-link">
-                        People
-                      </Link>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <Link href="/upcoming" className="navbar-link">
-                    Upcoming
-                  </Link>
-                </li>
-                <li>
-                  <Link href="#" className="navbar-link">
-                    About
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-          </header>
-          <main className="px-2 sm:px-4 md:px-8 lg:px-16 xl:px-32 mb-10">
-            {children}
-          </main>
-          <footer></footer>
-        </div>
+        <main>{children}</main>
       </body>
     </html>
   );
