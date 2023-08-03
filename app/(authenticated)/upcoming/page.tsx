70ce86b11a92423f387af3d2db2d8b9acccfce5f
aort <artashesano@gmail.com>
2023-08-03 04:22:19 +0400

    Update upcoming page to visually match other pages

--- app/(authenticated)/upcoming/page.tsx
+++ app/(authenticated)/upcoming/page.tsx
@@ -5,7 +5,7 @@ export default async function UpcomingMovies() {
   const movies = (await getUpcomingMovies()).results;
 
   return (
-    <div className="grid gap-16 grid-cols-4 ">
+    <div className="grid gap-4 md:gap-10 lg:gap-14 xl:gap-16 grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
       {movies?.map((movie, index) => (
         <MovieItem key={movie.id} movie={movie} index={index} />
       ))}
