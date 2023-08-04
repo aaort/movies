517bad95168fd8c7963298ff9589d163f955333a
aort <artashesano@gmail.com>
2023-08-04 19:08:25 +0400

    Add Account type

--- types.d.ts
+++ types.d.ts
@@ -49,3 +49,20 @@ type TV = Pick<
   first_air_date: string;
   origin_country: string[];
 };
+
+type Account = {
+  avatar: {
+    gravatar: {
+      hash: string;
+    };
+    tmdb: {
+      avatar_path: string;
+    };
+  };
+  id: number;
+  iso_639_1: string;
+  iso_3166_1: string;
+  name: string;
+  include_adult: boolean;
+  username: string;
+};
