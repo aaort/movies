32eac6b53a84a2e9fb90de63d8fee886923c4588
aort <artashesano@gmail.com>
2023-08-05 19:52:17 +0400

    Add CastPerson type

--- types.d.ts
+++ types.d.ts
@@ -109,3 +109,18 @@ type MovieDetails = {
   vote_average: number;
   vote_count: number;
 };
+
+type CastPerson = {
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: string;
+  cast_id: number;
+  character: string;
+  credit_id: string;
+  order: number;
+};
