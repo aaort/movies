786442b89b92e2f489be90547a61ee36d26b7fd2
aort <artashesano@gmail.com>
2023-08-06 01:50:56 +0400

    Improvements on Back title

--- app/components/Back.tsx
+++ app/components/Back.tsx
@@ -12,10 +12,12 @@ export default function Back({ to, classes }: Props) {
   const pathname = usePathname();
 
   const previousRoute = pathname.slice(0, pathname.lastIndexOf('/'));
+  const title = (to ? to.slice(to.lastIndexOf('/')) : previousRoute).slice(1);
 
   return (
-    <Link className={`${classes}`} href={to ?? previousRoute}>{`< ${
-      to ?? previousRoute
-    }`}</Link>
+    <Link
+      className={`capitalize ${classes}`}
+      href={to ?? previousRoute}
+    >{`< ${title}`}</Link>
   );
 }
