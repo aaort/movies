c9aa19b8c477308b7da0d8674faf5644954f1208
aort <artashesano@gmail.com>
2023-08-05 19:13:46 +0400

    Style improvements on movie details page

--- app/(authenticated)/movie/[slug]/page.tsx
+++ app/(authenticated)/movie/[slug]/page.tsx
@@ -27,28 +27,28 @@ export default async function MoviePage({ params: { slug: movieId } }: Props) {
         style={{ backgroundImage: `url(${imagePaths.backdrop})` }}
         className='bg-center bg-no-repeat h-[80svh] bg-cover grid place-items-center after:content-[" "] after:inset-0 after:absolute after:h-[80svh] after:opacity-60 after:bg-slate-950 after:z-10'
       >
-        <div className='flex text-white flex-col lg:flex-row gap-4 md:gap-8 lg:gap-16 w-[80%] mx-auto z-20'>
-          <div className='relative w-[100%] aspect-[1/1.5] overflow-clip rounded-md drop-shadow-2xl'>
+        <div className='flex text-white gap-4 md:gap-8 lg:gap-16 w-[85%] mx-auto z-20'>
+          <div className='relative w-[100%] min-w-[200px] aspect-[1/1.5] overflow-clip rounded-md drop-shadow-2xl'>
             <Image alt='Poster' fill src={imagePaths.poster} />
           </div>
 
           <div className='flex flex-col gap-10 justify-between'>
-            <div className='flex flex-col gap-4'>
+            <div className='space-y-8'>
               <div className='flex justify-between items-center'>
-                <div className='flex gap-4 items-center'>
-                  <h1>{movieDetails.title}</h1>
-                  <span className='text-neutral-200'>
+                <h1>
+                  {movieDetails.title}
+                  <span className='text-neutral-200 text-sm ml-4'>
                     {movieDetails.release_date}
                   </span>
-                </div>
+                </h1>
                 <span>Play Trailer</span>
               </div>
               <p>{movieDetails.tagline}</p>
             </div>
 
             <div className='space-y-4'>
-              <p>Overview</p>
-              <p>{movieDetails.overview}</p>
+              <p className='text-xl text-neutral-200'>Overview</p>
+              <p className='text-lg'>{movieDetails.overview}</p>
             </div>
           </div>
         </div>
