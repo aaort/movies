8a2f09435068fc2d477f523442676149463399df
aort <artashesano@gmail.com>
2023-08-05 11:22:25 +0400

    Enable flex-wrap on header nav

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -4,52 +4,52 @@ type Props = React.PropsWithChildren & {};
 
 export default function Layout({ children }: Props) {
   return (
-    <div className="flex flex-col h-full justify-between">
-      <header className="flex flex-wrap justify-end gap-y-4 items-center py-10 px-2 sm:px-4 md:px-8 lg:px-16 xl:px-32">
+    <div className='flex flex-col h-full justify-between'>
+      <header className='flex flex-wrap justify-end gap-y-4 items-center py-10 px-2 sm:px-4 md:px-8 lg:px-16 xl:px-32'>
         <nav>
-          <ul className="flex gap-2 sm:gap-4 md:gap-8 lg:gap-12 xl:gap-16 items-center">
-            <li className="relative inline-block group">
-              <button className="text-xl">Trending</button>
-              <ul className="hidden group-hover:flex flex-col gap-4 p-4 z-50 absolute bg-white border border-gray-300 rounded-md shadow-2xl">
-                <li className="">
-                  <Link href="/trending/movies" className="navbar-link">
+          <ul className='flex flex-wrap gap-2 sm:gap-4 md:gap-8 lg:gap-12 xl:gap-16 items-center'>
+            <li className='relative inline-block group'>
+              <button className='text-xl'>Trending</button>
+              <ul className='hidden group-hover:flex flex-col gap-4 p-4 z-50 absolute bg-white border border-gray-300 rounded-md shadow-2xl'>
+                <li className=''>
+                  <Link href='/trending/movies' className='navbar-link'>
                     Movies
                   </Link>
                 </li>
-                <li className="">
+                <li className=''>
                   <Link
-                    href="/trending/tv"
-                    className="navbar-link whitespace-nowrap w-full"
+                    href='/trending/tv'
+                    className='navbar-link whitespace-nowrap w-full'
                   >
                     TV Series
                   </Link>
                 </li>
-                <li className="">
-                  <Link href="/trending/people" className="navbar-link">
+                <li className=''>
+                  <Link href='/trending/people' className='navbar-link'>
                     People
                   </Link>
                 </li>
               </ul>
             </li>
             <li>
-              <Link href="/upcoming" className="navbar-link">
+              <Link href='/upcoming' className='navbar-link'>
                 Upcoming
               </Link>
             </li>
             <li>
-              <Link href="/favorite/movies" className="navbar-link">
+              <Link href='/favorite/movies' className='navbar-link'>
                 Favorite
               </Link>
             </li>
             <li>
-              <Link href="#" className="navbar-link">
+              <Link href='#' className='navbar-link'>
                 About
               </Link>
             </li>
           </ul>
         </nav>
       </header>
-      <main className="px-2 sm:px-4 md:px-8 lg:px-16 xl:px-32 mb-10">
+      <main className='px-2 sm:px-4 md:px-8 lg:px-16 xl:px-32 mb-10'>
         {children}
       </main>
       <footer></footer>
