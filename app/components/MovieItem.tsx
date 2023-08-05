d56854169c434bbbc16f5762f08d6ec1a450c987
aort <artashesano@gmail.com>
2023-08-06 03:18:59 +0400

    Use FavoriteButton in MovieItem

--- app/components/MovieItem.tsx
+++ app/components/MovieItem.tsx
@@ -1,8 +1,8 @@
-import addMovieToFavorite from '@/lib/api/addMovieToFavorite';
 import generateImageUrlByFilename from '@/lib/api/generateImageUrlByFilename';
-import { cookies } from 'next/headers';
 import Image from 'next/image';
 import Link from 'next/link';
+import FavoriteButton from './FavoriteButton';
+import { toggleFavorite } from './actions';
 
 type Props = {
   movie: Movie;
@@ -11,46 +11,22 @@ type Props = {
 };
 
 export default function MovieItem({ movie, index, favorite }: Props) {
-  async function addToFavorite() {
-    'use server';
-    const sessionId = cookies().get('session_id')?.value;
-    if (!sessionId) return;
-
-    try {
-      await addMovieToFavorite(movie.id, sessionId);
-    } catch (e) {
-      throw e;
-    }
-  }
-
   const posterPath = generateImageUrlByFilename(movie.poster_path);
+
+  const handleToggleFavorite = async (value: boolean) => {
+    'use server';
+    toggleFavorite(movie.id, value);
+  };
 
   return (
     <Link href={`/movie/${movie.id}`}>
       <div className='flex relative flex-col gap-4 bg-slate-300 text-slate-900 rounded-md overflow-clip cursor-pointer'>
         <div className="relative w-full aspect-[1/1.5] after:content-[' '] after:absolute after:w-full after:h-full after:bg-slate-900 after:bg-opacity-0 hover:after:bg-opacity-40 after:duration-300">
           <div className='flex flex-col gap-4 absolute top-4 right-4 z-10'>
-            <form action={addToFavorite}>
-              <button
-                className='p-2 bg-slate-200 rounded-full hover:opacity-90'
-                type='submit'
-              >
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                  strokeWidth='1.5'
-                  aria-checked={favorite}
-                  className='w-6 h-6 stroke-current aria-checked:fill-red-400 aria-checked:stroke-red-400'
-                >
-                  <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    d='M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z'
-                  />
-                </svg>
-              </button>
-            </form>
+            <FavoriteButton
+              checked={favorite}
+              onToggle={handleToggleFavorite}
+            />
           </div>
           <Image
             fill
