5fa571a970ab38a8e18e98f5028cf9974fc0c9eb
aort <artashesano@gmail.com>
2023-08-05 20:49:32 +0400

    Improvements on Cast

--- app/(authenticated)/movie/[slug]/sections/cast.tsx
+++ app/(authenticated)/movie/[slug]/sections/cast.tsx
@@ -10,13 +10,13 @@ export default async function Cast({ movieId }: Props) {
   const cast = (await getMovieCredits(movieId)).cast;
 
   return (
-    <div className='flex gap-10 mx-20 overflow-scroll'>
+    <ul className='flex gap-10 mx-20 overflow-scroll'>
       {cast.map((person) => {
         const profilePath = person.profile_path
           ? generateImageUrlByFilename(person.profile_path)
           : null;
         return (
-          <div
+          <li
             key={person.id}
             className='flex flex-col gap-4 bg-slate-200 rounded-md'
           >
@@ -25,13 +25,13 @@ export default async function Cast({ movieId }: Props) {
                 <Image alt={`${person.name}'s avatar`} fill src={profilePath} />
               ) : null}
             </div>
-            <div className='mx-2 space-y-4'>
+            <div className='flex flex-col justify-between mx-2 gap-4 mb-4 flex-1'>
               <p>{person.name}</p>
               <p>{person.known_for_department}</p>
             </div>
-          </div>
+          </li>
         );
       })}
-    </div>
+    </ul>
   );
 }
