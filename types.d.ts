8f5ce937bd18106de64f10e666c79d19ed7674af
aort <artashesano@gmail.com>
2023-08-06 15:53:31 +0400

    Add Video type

--- types.d.ts
+++ types.d.ts
@@ -108,3 +108,16 @@ type CrewPerson = Omit<CastPerson, 'cast_id' | 'character'> & {
   department: string;
   job: string;
 };
+
+type Video = {
+  iso_639_1: string;
+  iso_3166_1: string;
+  name: string;
+  key: string;
+  site: string;
+  size: number;
+  type: string;
+  official: boolean;
+  published_at: string;
+  id: string;
+};
