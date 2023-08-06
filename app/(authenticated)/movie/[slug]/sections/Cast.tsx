be9ad1037ca42ac028fcdfa83ab31e3ed76fe1c3
aort <artashesano@gmail.com>
2023-08-06 19:23:20 +0400

    Improvements on Cast

--- app/(authenticated)/movie/[slug]/sections/Cast.tsx
+++ app/(authenticated)/movie/[slug]/sections/Cast.tsx
@@ -1,6 +1,7 @@
 import generateImageUrlByFilename from '@/lib/api/generateImageUrlByFilename';
 import getMovieCredits from '@/lib/api/getMovieCredits';
 import Image from 'next/image';
+import Link from 'next/link';
 
 type Props = {
   movieId: number;
@@ -16,20 +17,23 @@ export default async function Cast({ movieId }: Props) {
           ? generateImageUrlByFilename(person.profile_path)
           : null;
         return (
-          <li
-            key={person.id}
-            className='flex flex-col gap-4 bg-slate-200 rounded-md'
-          >
-            <div className='relative max-w-[300px] min-w-[180px] aspect-[1/1.5] rounded-tl-md rounded-tr-md overflow-clip'>
-              {profilePath ? (
-                <Image alt={`${person.name}'s avatar`} fill src={profilePath} />
-              ) : null}
-            </div>
-            <div className='flex flex-col justify-between mx-2 gap-4 mb-4 flex-1'>
-              <p>{person.name}</p>
-              <p>{person.known_for_department}</p>
-            </div>
-          </li>
+          <Link key={person.id} href='#'>
+            <li className='flex flex-col gap-4 bg-slate-600 text-slate-100 rounded-md'>
+              <div className='relative max-w-[300px] min-w-[180px] aspect-[1/1.5] rounded-tl-md rounded-tr-md overflow-clip'>
+                {profilePath ? (
+                  <Image
+                    alt={`${person.name}'s avatar`}
+                    fill
+                    src={profilePath}
+                  />
+                ) : null}
+              </div>
+              <div className='flex flex-col justify-between mx-4 mb-4 flex-1'>
+                <h3>{person.name}</h3>
+                <p className='text-slate-300'>{person.known_for_department}</p>
+              </div>
+            </li>
+          </Link>
         );
       })}
     </ul>
