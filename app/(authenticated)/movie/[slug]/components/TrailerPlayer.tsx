3cac4957cf0bd80758d0b6b06826c0c735d707f1
aort <artashesano@gmail.com>
2023-08-07 01:11:00 +0400

    Fix jumping layout in TrailerPlayer

--- app/(authenticated)/movie/[slug]/components/TrailerPlayer.tsx
+++ app/(authenticated)/movie/[slug]/components/TrailerPlayer.tsx
@@ -13,28 +13,29 @@ export default function TrailerPlayer({ videoKey }: Props) {
     setIsOpen(true);
   };
 
-  if (!isOpen) {
-    return <button onClick={handleOpen}>Play Trailer</button>;
-  }
-
   const handleClose = () => {
     setIsOpen(false);
   };
 
   return (
-    <div
-      className='modal w-full h-full grid place-items-center'
-      onClick={handleClose}
-    >
-      <iframe
-        width={727}
-        height={409}
-        src={`https://youtube.com/embed/${videoKey}`}
-        title='YouTube video player'
-        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-        allowFullScreen
-        className='rounded-md'
-      />
-    </div>
+    <>
+      <button onClick={handleOpen}>Play Trailer</button>
+      {isOpen && (
+        <div
+          className='modal w-full h-full grid place-items-center'
+          onClick={handleClose}
+        >
+          <iframe
+            width={727}
+            height={409}
+            src={`https://youtube.com/embed/${videoKey}`}
+            title='YouTube video player'
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+            allowFullScreen
+            className='rounded-md'
+          />
+        </div>
+      )}
+    </>
   );
 }
