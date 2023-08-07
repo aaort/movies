f69a80f6c045a456e1fa52adb5459fa4b98cec8d
aort <artashesano@gmail.com>
2023-08-07 16:56:06 +0400

    Export getReviews function from Reviews

--- app/(authenticated)/movie/[slug]/sections/Reviews.tsx
+++ app/(authenticated)/movie/[slug]/sections/Reviews.tsx
@@ -5,15 +5,20 @@ type Props = {
   movieId: string | number;
 };
 
-type GetResultType = ResultType<Review> & {
+type GetReviewsResultType = ResultType<Review> & {
   id: number;
   total_pages: number;
   total_results: number;
 };
 
+export async function getReviews(
+  movieId: string | number
+): Promise<GetReviewsResultType> {
+  return await get<GetReviewsResultType>(`movie/${movieId}/reviews`);
+}
+
 export default async function Reviews({ movieId }: Props) {
-  const reviews = (await get<GetResultType>(`movie/${movieId}/reviews`))
-    .results;
+  const reviews = (await getReviews(movieId)).results;
 
   return (
     <ul className='max-w-full inline-flex gap-10 overflow-x-auto'>
