f46a7fa5e1f5e55fe00126a4e292f0023c206ce2
aort <artashesano@gmail.com>
2023-08-07 13:27:47 +0400

    Add Review type

--- types.d.ts
+++ types.d.ts
@@ -130,3 +130,18 @@ type ExternalIds = {
   instagram_id: string;
   twitter_id: string;
 };
+
+type Review = {
+  author: string;
+  author_details: {
+    name: string;
+    username: string;
+    avatar_path: string | null;
+    rating: number;
+  };
+  content: string;
+  created_at: string;
+  id: string;
+  updated_at: string;
+  url: string;
+};
