80a3ec0e915fb64f948ae52a4ee02a40135bd64b
aort <artashesano@gmail.com>
2023-08-07 16:55:32 +0400

    Back component accepts title now

--- app/components/Back.tsx
+++ app/components/Back.tsx
@@ -7,18 +7,20 @@ import { BsArrowLeftShort } from 'react-icons/bs';
 type Props = {
   to?: string;
   classes?: string;
+  title?: string;
 };
 
-export default function Back({ to, classes }: Props) {
+export default function Back({ to, classes, title }: Props) {
   const pathname = usePathname();
 
   const previousRoute = pathname.slice(0, pathname.lastIndexOf('/'));
-  const title = (to ? to.slice(to.lastIndexOf('/')) : previousRoute).slice(1);
+  const _title =
+    title ?? (to ? to.slice(to.lastIndexOf('/')) : previousRoute).slice(1);
 
   return (
     <Link className={`capitalize ${classes}`} href={to ?? previousRoute}>
       <div className='flex gap-2 items-center'>
-        <BsArrowLeftShort className='w-6 h-6' /> <span>{title}</span>
+        <BsArrowLeftShort className='w-6 h-6' /> <span>{_title}</span>
       </div>
     </Link>
   );
