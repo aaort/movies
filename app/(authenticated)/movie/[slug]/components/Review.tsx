30f11b7bb740a3eb48a7338a26bf3cea4c47b2cb
aort <artashesano@gmail.com>
2023-08-07 16:31:11 +0400

    Do not show rating if it's null and some style improvements on Review

--- app/(authenticated)/movie/[slug]/components/Review.tsx
+++ app/(authenticated)/movie/[slug]/components/Review.tsx
@@ -27,20 +27,26 @@ export default function Review({ review }: Props) {
   } ${writtenDate.getDay()}, ${writtenDate.getFullYear()}`;
 
   return (
-    <li className='flex-none w-full max-h-[300px] overflow-hidden flex flex-col gap-10 rounded-md border-2 border-slate-100 p-4'>
+    <li className='flex-none basis-full w-full max-h-[300px] overflow-hidden flex flex-col gap-10 rounded-md border-2 border-slate-100 p-4'>
       <div className='space-y-2'>
         <h3>
           By <span className='font-bold underline'>{review.author}</span>
         </h3>
         <div className='flex gap-2 items-center'>
-          <span className='flex items-center gap-1 w-fit px-2 rounded-full bg-slate-800 text-slate-200'>
-            <AiFillStar className='fill-slate-100' />
-            {review.author_details.rating}
-          </span>
+          {review.author_details.rating && (
+            <span className='flex items-center gap-1 w-fit px-2 rounded-full bg-slate-800 text-slate-200'>
+              <AiFillStar className='fill-slate-100' />
+              {review.author_details.rating}
+            </span>
+          )}
           <span className='text-slate-700'>Written at - {writtenStr}</span>
         </div>
       </div>
-      <span className='overflow-ellipsis'>{review.content}</span>
+      <div className='overflow-hidden'>
+        <span className='overflow-ellipsis break-words line-clamp-6'>
+          {review.content}
+        </span>
+      </div>
     </li>
   );
 }
