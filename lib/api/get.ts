738545b47767f60afb065dc830eafd5d2b7f7ba3
aort <artashesano@gmail.com>
2023-08-08 11:03:47 +0400

    Write docs and comments for get method

--- lib/api/get.ts
+++ lib/api/get.ts
@@ -2,6 +2,15 @@ const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 const readApiKey = process.env.NEXT_PUBLIC_API_READ_ACCESS_KEY;
 const apiKey = process.env.NEXT_PUBLIC_API_KEY;
 
+// Utility function for fetching exclusively from TMDB API with custom configurations
+// such as headers, optional api key and base url
+/**
+ *
+ * @param endpoint - Url that request should be made to with TMDB api base url prefixed
+ * @param options - RequestInit object, headers's Authorization is included by default but can be overwritten
+ * @param includeApiKey - Weither include api key as query param or not
+ * @returns Promise of either provided type or null | undefined
+ */
 export default async function get<T>(
   endpoint: string,
   options?: RequestInit,
