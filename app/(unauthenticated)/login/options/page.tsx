44fd6ab4d38c10291b91e7d71f08cb709c70a585
aort <artashesano@gmail.com>
2023-08-09 17:27:31 +0400

    Add more content in the login form

--- app/(unauthenticated)/login/options/page.tsx
+++ app/(unauthenticated)/login/options/page.tsx
@@ -11,7 +11,30 @@ export default async function Options() {
 
   return (
     <section aria-label='auth-options-section'>
-      <div className='flex flex-col absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%] gap-10'>
+      <div className='flex flex-col gap-10 absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%] shadow-lg shadow-gray-200/90 rounded-xl p-10'>
+        <section className='mx-auto text-center space-y-4'>
+          <b>
+            <h1>Movies</h1>
+          </b>
+          <p>
+            Login to your <Link href='https://www.themoviedb.org/'>TMDB</Link>{' '}
+            account (or create one) to access the full range of capabilities of
+            the website
+          </p>
+          <p>
+            <strong>OR</strong>
+          </p>
+          <p>
+            Login as a guest to quickly view what is going on in the cinema
+            world
+            <span className='italic'>
+              {' '}
+              {
+                "(Guest users will have limited access to the website's capabilities)"
+              }
+            </span>
+          </p>
+        </section>
         <Link className='button' href={loginUrl} aria-disabled={!requestToken}>
           Login
         </Link>
