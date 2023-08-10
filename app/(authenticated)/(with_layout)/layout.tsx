955c68cef83986bf175f84ef4b522c0f72d25648
aort <artashesano@gmail.com>
2023-08-10 15:14:11 +0400

    Include search bar for trending movies

--- app/(authenticated)/(with_layout)/layout.tsx
+++ app/(authenticated)/(with_layout)/layout.tsx
@@ -5,7 +5,7 @@ type Props = React.PropsWithChildren & {};
 export default function Layout({ children }: Props) {
   return (
     <div className='flex flex-col h-full justify-between'>
-      <Header />
+      <Header includeSearch />
       <main className='px-2 sm:px-4 md:px-8 lg:px-16 xl:px-32 mb-10'>
         {children}
       </main>
