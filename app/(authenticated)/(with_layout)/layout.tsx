2899cf490e16b3e6a6adda7275146bae79e9cecc
aort <artashesano@gmail.com>
2023-08-10 13:43:44 +0400

    Extract Header component

--- app/(authenticated)/(with_layout)/layout.tsx
+++ app/(authenticated)/(with_layout)/layout.tsx
@@ -1,56 +1,11 @@
-import Search from '@/app/components/Search';
-import Link from 'next/link';
+import Header from '@/app/components/Header';
 
 type Props = React.PropsWithChildren & {};
 
 export default function Layout({ children }: Props) {
   return (
     <div className='flex flex-col h-full justify-between'>
-      <header className='flex flex-wrap justify-between gap-4 items-center py-10 px-2 sm:px-4 md:px-8 lg:px-16 xl:px-32'>
-        <Search />
-        <nav>
-          <ul className='flex flex-wrap gap-2 sm:gap-4 md:gap-8 lg:gap-12 xl:gap-16 items-center'>
-            <li className='relative inline-block group'>
-              <button className='text-xl'>Trending</button>
-              <ul className='hidden group-hover:flex flex-col gap-4 p-4 z-50 absolute bg-white border border-gray-300 rounded-md shadow-2xl'>
-                <li className=''>
-                  <Link href='/trending/movies' className='navbar-link'>
-                    Movies
-                  </Link>
-                </li>
-                <li className=''>
-                  <Link
-                    href='/trending/tv'
-                    className='navbar-link whitespace-nowrap w-full'
-                  >
-                    TV Series
-                  </Link>
-                </li>
-                <li className=''>
-                  <Link href='/trending/people' className='navbar-link'>
-                    People
-                  </Link>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <Link href='/upcoming' className='navbar-link'>
-                Upcoming
-              </Link>
-            </li>
-            <li>
-              <Link href='/favorite/movies' className='navbar-link'>
-                Favorite
-              </Link>
-            </li>
-            <li>
-              <Link href='#' className='navbar-link'>
-                About
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
+      <Header />
       <main className='px-2 sm:px-4 md:px-8 lg:px-16 xl:px-32 mb-10'>
         {children}
       </main>
---
+++ app/components/Header.tsx
@@ -0,0 +1,56 @@
+import Link from 'next/link';
+import Search from './Search';
+
+type Props = {
+  includeSearch?: boolean;
+};
+
+export default async function Header({ includeSearch = false }: Props) {
+  return (
+    <header className='flex flex-wrap justify-between gap-4 items-center py-10 px-2 sm:px-4 md:px-8 lg:px-16 xl:px-32'>
+      {includeSearch && <Search />}
+      <nav>
+        <ul className='flex flex-wrap gap-2 sm:gap-4 md:gap-8 lg:gap-12 xl:gap-16 items-center'>
+          <li className='relative inline-block group'>
+            <button className='text-xl'>Trending</button>
+            <ul className='hidden group-hover:flex flex-col gap-4 p-4 z-50 absolute bg-white border border-gray-300 rounded-md shadow-2xl'>
+              <li className=''>
+                <Link href='/trending/movies' className='navbar-link'>
+                  Movies
+                </Link>
+              </li>
+              <li className=''>
+                <Link
+                  href='/trending/tv'
+                  className='navbar-link whitespace-nowrap w-full'
+                >
+                  TV Series
+                </Link>
+              </li>
+              <li className=''>
+                <Link href='/trending/people' className='navbar-link'>
+                  People
+                </Link>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <Link href='/upcoming' className='navbar-link'>
+              Upcoming
+            </Link>
+          </li>
+          <li>
+            <Link href='/favorite/movies' className='navbar-link'>
+              Favorite
+            </Link>
+          </li>
+          <li>
+            <Link href='#' className='navbar-link'>
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
