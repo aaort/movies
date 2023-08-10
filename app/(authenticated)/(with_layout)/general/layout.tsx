5f2f07192fadd94195d2999b4887edabd99738e1
aort <artashesano@gmail.com>
2023-08-11 00:28:49 +0400

    Add page and layout for general

--- app/(authenticated)/(with_layout)/general/layout.tsx
+++ app/(authenticated)/(with_layout)/general/layout.tsx
@@ -1,15 +1,25 @@
 import Header from '@/app/components/Header';
+import Link from 'next/link';
+import { BsArrowRightShort } from 'react-icons/bs';
 
 type Props = React.PropsWithChildren & {};
 
 export default function Layout({ children }: Props) {
   return (
     <div className='flex flex-col h-full justify-between'>
-      <Header />
+      <Header leftComponent={<SearchButton />} />
       <main className='px-2 sm:px-4 md:px-8 lg:px-16 xl:px-32 mb-10'>
         {children}
       </main>
       <footer></footer>
     </div>
+  );
+}
+
+function SearchButton() {
+  return (
+    <Link href='/search' className='button'>
+      Search <BsArrowRightShort className='inline-block w-5 h-5' />
+    </Link>
   );
 }
--- app/(authenticated)/(with_layout)/general/page.tsx
+++ app/(authenticated)/(with_layout)/general/page.tsx
@@ -1,3 +1,23 @@
+import MovieGridCard from '@/app/components/cards/MovieGridCard';
+import get from '@/lib/api/get';
+
 export default async function GeneralPage() {
-  return <h1>General Page</h1>;
+  const movies = (await get<ResultType<Movie>>('trending/movie/week'))?.results;
+
+  return (
+    <div className='space-y-10'>
+      <section>
+        <h2 className='mb-8 text-xl'>Trending</h2>
+        <ul className='flex gap-14 overflow-x-auto'>
+          {movies?.map((movie, i) => (
+            <MovieGridCard key={movie.id} movie={movie} index={i} />
+          ))}
+        </ul>
+      </section>
+
+      <section>
+        <h2></h2>
+      </section>
+    </div>
+  );
 }
