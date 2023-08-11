7d3d8ce2ecedb50c23fea6a1e0f69651326c827a
aort <artashesano@gmail.com>
2023-08-11 15:14:10 +0400

    Wrap GridList with ul

--- app/components/GridList.tsx
+++ app/components/GridList.tsx
@@ -2,8 +2,8 @@ type Props = React.PropsWithChildren & {};
 
 export default function GridList({ children }: Props) {
   return (
-    <div className="grid gap-4 md:gap-10 lg:gap-14 xl:gap-16 grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+    <ul className='grid gap-4 md:gap-10 lg:gap-14 xl:gap-16 grid-cols-2 md:grid-cols-3 xl:grid-cols-4'>
       {children}
-    </div>
+    </ul>
   );
 }
