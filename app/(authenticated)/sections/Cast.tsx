13a8b5aa0d176bdea02fac548aa96dc2badd93ba
aort <artashesano@gmail.com>
2023-08-11 17:53:58 +0400

    Use PersonGridCard in Cast

--- app/(authenticated)/sections/Cast.tsx
+++ app/(authenticated)/sections/Cast.tsx
@@ -1,7 +1,5 @@
-import generateImageUrlByFilename from '@/lib/generateImageUrlByFilename';
+import PersonGridCard from '@/app/components/cards/PersonGridCard';
 import get from '@/lib/api/get';
-import Image from 'next/image';
-import Link from 'next/link';
 
 type Props = {
   forPath: string;
@@ -11,31 +9,12 @@ export default async function Cast({ forPath }: Props) {
   const cast = (await get<Credits>(forPath))?.cast;
 
   return (
-    <ul className='flex gap-10 overflow-scroll pb-4'>
-      {cast?.map((person) => {
-        const profilePath = person.profile_path
-          ? generateImageUrlByFilename(person.profile_path)
-          : null;
-        return (
-          <Link key={person.id} href='#'>
-            <li className='flex flex-col h-full gap-4 bg-slate-600 text-slate-100 rounded-md'>
-              <div className='relative max-w-[300px] min-w-[180px] aspect-[1/1.5] rounded-tl-md rounded-tr-md overflow-clip'>
-                {profilePath ? (
-                  <Image
-                    alt={`${person.name}'s avatar`}
-                    fill
-                    src={profilePath}
-                  />
-                ) : null}
-              </div>
-              <div className='flex flex-col justify-between mx-4 mb-4'>
-                <h3>{person.name}</h3>
-                <p className='text-slate-300'>{person.known_for_department}</p>
-              </div>
-            </li>
-          </Link>
-        );
-      })}
+    <ul className='flex gap-10 overflow-x-auto pb-8'>
+      {cast?.map((person, i) => (
+        <li key={person.id} className='min-w-[15vw]'>
+          <PersonGridCard person={person} index={i} />
+        </li>
+      ))}
     </ul>
   );
 }
