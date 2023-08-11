0cfe76d6469922cde8df95ba9629aefe2222e745
aort <artashesano@gmail.com>
2023-08-11 13:13:05 +0400

    Add tvs section in the general page

--- app/(authenticated)/(with_layout)/general/page.tsx
+++ app/(authenticated)/(with_layout)/general/page.tsx
@@ -1,14 +1,16 @@
 import MovieGridCard from '@/app/components/cards/MovieGridCard';
+import TVGridCard from '@/app/components/cards/TVGridCard';
 import get from '@/lib/api/get';
 
 export default async function GeneralPage() {
   const movies = (await get<ResultType<Movie>>('trending/movie/week'))?.results;
+  const tvs = (await get<ResultType<TV>>('trending/tv/week'))?.results;
 
   return (
     <div className='space-y-10'>
       <section>
-        <h2 className='mb-8 text-xl'>Trending</h2>
-        <ul className='flex gap-14 overflow-x-auto'>
+        <h2 className='mb-8 text-xl'>Trending - Movies</h2>
+        <ul className='flex gap-14 overflow-x-auto pb-4'>
           {movies?.map((movie, i) => (
             <MovieGridCard key={movie.id} movie={movie} index={i} />
           ))}
@@ -16,7 +18,12 @@ export default async function GeneralPage() {
       </section>
 
       <section>
-        <h2></h2>
+        <h2 className='mb-8 text-xl'>Trending - TVs</h2>
+        <ul className='flex gap-14 overflow-x-auto pb-4'>
+          {tvs?.map((tv, i) => (
+            <TVGridCard key={tv.id} tv={tv} index={i} />
+          ))}
+        </ul>
       </section>
     </div>
   );
