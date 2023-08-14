c09805e2f7ea54238fd7eea89a2e7e046084ac64
aort <artashesano@gmail.com>
2023-08-14 17:46:07 +0400

    Implement Reviews with inline-flex

--- app/(authenticated)/sections/Cast.tsx
+++ app/(authenticated)/sections/Cast.tsx
@@ -9,7 +9,7 @@ export default async function Cast({ forPath }: Props) {
   const cast = (await get<Credits>(forPath))?.cast;
 
   return (
-    <ul className='flex gap-10 overflow-x-auto pb-8'>
+    <ul className='max-w-full inline-flex gap-x-32 overflow-x-auto pb-8'>
       {cast?.map((person, i) => (
         <li key={person.id} className='min-w-[15vw]'>
           <PersonCard person={person} index={i} />
