6b331013179184f8f514cdbc6628b03188ad6299
aort <artashesano@gmail.com>
2023-08-14 16:01:17 +0400

    Apply appropriate styles for move, tv and person cards

--- app/components/cards/MovieCard.tsx
+++ app/components/cards/MovieCard.tsx
@@ -30,33 +30,33 @@ export default async function MovieCard({ movie, index }: Props) {
   };
 
   return (
-    <Link href={`/movie/${movie.id}`} className='min-w-[20vw]'>
-      <div className='grid-card group h-full'>
-        <div className="relative w-full aspect-[1/1.5] after:content-[' '] after:absolute after:w-full after:h-full after:bg-slate-900 after:bg-opacity-0 group-hover:after:bg-opacity-40 after:duration-300">
-          <div className='hidden absolute top-4 right-4 z-10 group-hover:block space-x-4'>
-            <FavoriteButton
-              checked={isFavorite}
-              onToggle={handleToggleFavorite}
-            />
-            <WatchlistButton
-              checked={isInWatchlist}
-              onToggle={handleToggleWatchlist}
-            />
-          </div>
-          <Image
-            fill
-            alt=''
-            src={posterPath}
-            sizes='(min-width: 1280px) calc(25vw - 112px), (min-width: 1040px) calc(25vw - 80px), (min-width: 780px) calc(25vw - 64px), calc(24.13vw - 49px)'
-            className='object-fill'
-            priority={index < 6}
+    <Link href={`/movie/${movie.id}`} className='min-w-[20vw] grid-card group'>
+      <div className="relative w-full aspect-[1/1.5] after:content-[' '] after:absolute after:w-full after:h-full after:bg-slate-900 after:bg-opacity-0 group-hover:after:bg-opacity-40 after:duration-300">
+        <div className='hidden absolute top-4 right-4 z-10 group-hover:block space-x-4'>
+          <FavoriteButton
+            checked={isFavorite}
+            onToggle={handleToggleFavorite}
+          />
+          <WatchlistButton
+            checked={isInWatchlist}
+            onToggle={handleToggleWatchlist}
           />
         </div>
-        <p className='mx-4 mb-4 break-normal'>
-          {movie.title}
-          <br />
-          {movie.release_date}
-        </p>
+        <Image
+          fill
+          alt=''
+          src={posterPath}
+          sizes='(min-width: 1280px) calc(25vw - 112px), (min-width: 1040px) calc(25vw - 80px), (min-width: 780px) calc(25vw - 64px), calc(24.13vw - 49px)'
+          className='object-fill'
+          priority={index < 6}
+        />
+        <div className='grid-card-text-box'>
+          <p className='mx-4 mb-4 break-normal'>
+            {movie.title}
+            <br />
+            {movie.release_date}
+          </p>
+        </div>
       </div>
     </Link>
   );
--- app/components/cards/PersonCard.tsx
+++ app/components/cards/PersonCard.tsx
@@ -15,27 +15,25 @@ export default function PersonCard({ person, index }: Props) {
     : null;
 
   return (
-    <Link href='#'>
-      <div className='grid-card h-full'>
-        <div
-          className={`relative w-full aspect-[1/1.5] grid place-items-center ${
-            !fullProfilePath ? 'border-b-[1px] border-slate-400' : ''
-          }`}
-        >
-          {fullProfilePath ? (
-            <Image
-              fill
-              alt={`${person.name}'s profile picture`}
-              src={fullProfilePath}
-              sizes='(min-width: 1280px) calc(25vw - 112px), (min-width: 1040px) calc(25vw - 80px), (min-width: 780px) calc(25vw - 64px), calc(24.13vw - 49px)'
-              className='object-fill'
-              priority={index < 6}
-            />
-          ) : (
-            <BsFillPersonFill className='w-[80%] h-[80%] ' />
-          )}
-        </div>
-        <div className='mx-4 mb-4'>
+    <Link href='#' className='min-w-[20vw] grid-card group'>
+      <div
+        className={`relative w-full aspect-[1/1.5] grid place-items-center ${
+          !fullProfilePath ? 'border-b-[1px] border-slate-400' : ''
+        }`}
+      >
+        {fullProfilePath ? (
+          <Image
+            fill
+            alt={`${person.name}'s profile picture`}
+            src={fullProfilePath}
+            sizes='(min-width: 1280px) calc(25vw - 112px), (min-width: 1040px) calc(25vw - 80px), (min-width: 780px) calc(25vw - 64px), calc(24.13vw - 49px)'
+            className='object-fill'
+            priority={index < 6}
+          />
+        ) : (
+          <BsFillPersonFill className='w-[80%] h-[80%] ' />
+        )}
+        <div className='px-4 pb-4 grid-card-text-box'>
           <p>{person.name}</p>
           <p>{person.known_for_department}</p>
         </div>
