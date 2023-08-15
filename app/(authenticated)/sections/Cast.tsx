81b3111cfb009989e82cdfc5956f0d4c9ad43bcd
aort <artashesano@gmail.com>
2023-08-15 20:25:47 +0400

    Set min-width for cast members in the Cast

--- app/(authenticated)/sections/Cast.tsx
+++ app/(authenticated)/sections/Cast.tsx
@@ -9,9 +9,9 @@ export default async function Cast({ forPath }: Props) {
   const cast = (await get<Credits>(forPath))?.cast;
 
   return (
-    <ul className='max-w-full inline-flex gap-x-32 overflow-x-auto pb-8'>
+    <ul className='max-w-full inline-flex gap-x-10 overflow-x-auto pb-8'>
       {cast?.map((person, i) => (
-        <li key={person.id} className='min-w-[15vw]'>
+        <li key={person.id} className='min-w-[20vw]'>
           <PersonCard person={person} index={i} />
         </li>
       ))}
