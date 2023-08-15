4931ffd9b63fa2bacee3f3d2b22997863ed87c57
aort <artashesano@gmail.com>
2023-08-15 15:22:05 +0400

    Implement Pagination component

---
+++ app/_components/Pagination.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+type Props = {
+  page: number;
+  totalPages: number;
+};
+
+export default function Pagination({ page, totalPages }: Props) {
+  const pathname = usePathname();
+  const pages = Array.from([
+    page - 2,
+    page - 1,
+    page,
+    page + 1,
+    page + 2,
+  ]).filter((_page) => _page > 0 && _page <= totalPages);
+
+  return (
+    <ul className='flex gap-2'>
+      {pages.map((_page) => (
+        <li key={_page}>
+          <Link
+            href={`${pathname}?page=${_page}`}
+            aria-selected={page === _page}
+            className='hover:underline p-2 aria-selected:underline'
+          >
+            {_page}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
