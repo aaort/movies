a78330ad2efca1961bfa3888792189b918415df2
aort <artashesano@gmail.com>
2023-08-18 17:03:32 +0400

    FIx spacing and colors in tv details page

--- app/(authenticated)/tv/[tvId]/page.tsx
+++ app/(authenticated)/tv/[tvId]/page.tsx
@@ -54,17 +54,14 @@ export default async function TVPage({ params: { tvId } }: Props) {
   return (
     <>
       <aside>
-        <Back
-          classes='text-primary-100 absolute left-2 top-2 z-50'
-          title='TVs'
-        />
+        <Back classes='text-primary-100 absolute left-2 top-2' title='TVs' />
       </aside>
       <section className='space-y-10 mb-10'>
         <div
           style={{ backgroundImage: `url(${imagePaths.backdrop})` }}
           className='bg-center aspect-video bg-no-repeat bg-cover grid place-items-center text-primary-100'
         >
-          <div className='w-full h-full flex  justify-center items-center backdrop-brightness-[0.4] p-4 md:p-10'>
+          <div className='w-full h-full flex justify-center items-center backdrop-brightness-[0.4] p-4 md:p-10'>
             <div className='flex flex-col items-start sm:flex-row text-white gap-4 md:gap-8 lg:gap-16 mx-4 z-20'>
               <div className='relative w-full min-w-[15vw] max-w-[30vw] aspect-[1/1.5] overflow-clip rounded-md drop-shadow-2xl'>
                 <Image
@@ -106,50 +103,52 @@ export default async function TVPage({ params: { tvId } }: Props) {
           </div>
         </div>
 
-        <section className='flex flex-col md:flex-row gap-10 mx-10 justify-between'>
-          <section className='space-y-10 overflow-hidden order-last md:order-first'>
-            <h2 className='text-xl font-bold'>Cast</h2>
-            <Cast forPath={`tv/${tvId}/credits`} />
+        <div className='page-space-m'>
+          <section className='flex flex-col md:flex-row gap-10 justify-between'>
+            <section className='space-y-10 overflow-hidden order-last md:order-first'>
+              <h2 className='text-xl font-bold'>Cast</h2>
+              <Cast forPath={`tv/${tvId}/credits`} />
+            </section>
+            <aside className='order-first md:order-last'>
+              <ExternalLinks path={`tv/${tvId}/external_ids`} />
+            </aside>
           </section>
-          <aside className='order-first md:order-last'>
-            <ExternalLinks path={`tv/${tvId}/external_ids`} />
-          </aside>
-        </section>
 
-        <section>
-          <dl className='flex flex-wrap gap-[10%] gap-y-10 mx-10'>
-            <div className='space-y-2'>
-              <dt>Status</dt>
-              <dd className='text-neutral-500 text-sm'>{tv.status}</dd>
-            </div>
-            <div className='space-y-2'>
-              <dt>Original Language</dt>
-              <dd className='text-neutral-500 text-sm'>
-                {tv.original_language}
-              </dd>
-            </div>
-            <div className='space-y-2'>
-              <dt>Genres</dt>
-              <dd className='text-neutral-500 text-sm'>
-                <ul className='space-y-2'>
-                  {tv.genres.map((genre) => (
-                    <li key={genre.id}>{genre.name}</li>
-                  ))}
-                </ul>
-              </dd>
-            </div>
-            <div className='space-y-2'>
-              <dt>Popularity</dt>
-              <dd className='text-neutral-500 text-sm'>{tv.popularity}</dd>
-            </div>
-          </dl>
-        </section>
+          <section>
+            <dl className='flex flex-wrap gap-[10%] gap-y-10 '>
+              <div className='space-y-2'>
+                <dt>Status</dt>
+                <dd className='text-primary-500 text-sm'>{tv.status}</dd>
+              </div>
+              <div className='space-y-2'>
+                <dt>Original Language</dt>
+                <dd className='text-primary-500 text-sm'>
+                  {tv.original_language}
+                </dd>
+              </div>
+              <div className='space-y-2'>
+                <dt>Genres</dt>
+                <dd className='text-primary-500 text-sm'>
+                  <ul className='space-y-2'>
+                    {tv.genres.map((genre) => (
+                      <li key={genre.id}>{genre.name}</li>
+                    ))}
+                  </ul>
+                </dd>
+              </div>
+              <div className='space-y-2'>
+                <dt>Popularity</dt>
+                <dd className='text-primary-500 text-sm'>{tv.popularity}</dd>
+              </div>
+            </dl>
+          </section>
 
-        <div className='mx-10 space-y-10'>
-          <hr />
+          <div className='mt-10 space-y-10'>
+            <hr />
 
-          <h2 className='text-xl font-bold'>Reviews</h2>
-          <Reviews movieId={tvId} />
+            <h2 className='text-xl font-bold'>Reviews</h2>
+            <Reviews movieId={tvId} />
+          </div>
         </div>
       </section>
     </>
