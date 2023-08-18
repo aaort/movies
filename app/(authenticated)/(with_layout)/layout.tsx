504c0171b0835149c58bf7e9c80a164f29136179
aort <artashesano@gmail.com>
2023-08-18 15:00:52 +0400

    Use defined class for horizontal spacing in the layout and update search link color

--- app/(authenticated)/(with_layout)/layout.tsx
+++ app/(authenticated)/(with_layout)/layout.tsx
@@ -8,9 +8,7 @@ export default function Layout({ children }: Props) {
   return (
     <div className='flex flex-col h-full justify-between'>
       <Header leftComponent={<SearchButton />} />
-      <main className='px-2 sm:px-4 md:px-8 lg:px-16 xl:px-32 mb-10'>
-        {children}
-      </main>
+      <main className='px-2 page-horizontal-space mb-10'>{children}</main>
       <footer></footer>
     </div>
   );
@@ -18,7 +16,10 @@ export default function Layout({ children }: Props) {
 
 function SearchButton() {
   return (
-    <Link href='/search' className='inline-block align-middle group'>
+    <Link
+      href='/search'
+      className='inline-block align-middle group text-slate-500/70'
+    >
       Search
       <BsArrowRightShort className='inline-block w-5 h-5 opacity-0 -translate-x-1 group-hover:opacity-100 group-hover:translate-x-0 duration-300' />
     </Link>
