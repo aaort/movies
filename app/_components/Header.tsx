98a7e69553c4e9e7775b4a027926753a19a3e685
aort <artashesano@gmail.com>
2023-08-21 19:33:37 +0400

    Remove repetitive code in the Header

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -20,49 +20,18 @@ export default async function Header({
           <Drawer />
         </div>
         <ul className='hidden md:flex flex-wrap gap-2 sm:gap-4 md:gap-8 lg:gap-12 xl:gap-16 items-center'>
-          <li>
-            <Link href='/general' className='navbar-link'>
-              General
-            </Link>
-          </li>
-          <li className='relative inline-block group'>
+          <NavbarLink href='/general'>General</NavbarLink>
+          <ul className='relative inline-block group'>
             <button className='navbar-link after:hidden'>Trending</button>
             <ul className='hidden group-hover:flex [&_a]:text-[1rem] flex-col gap-4 p-4 z-50 absolute bg-primary-50 border border-primary-300 rounded-md shadow-2xl'>
-              <li>
-                <Link href='/trending/movies' className='navbar-link'>
-                  Movies
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href='/trending/tvs'
-                  className='navbar-link whitespace-nowrap w-full'
-                >
-                  TV Series
-                </Link>
-              </li>
-              <li>
-                <Link href='/trending/people' className='navbar-link'>
-                  People
-                </Link>
-              </li>
+              <NavbarLink href='/trending/movies'>Movies</NavbarLink>
+              <NavbarLink href='/trending/tvs'>TV Series</NavbarLink>
+              <NavbarLink href='/trending/people'>People</NavbarLink>
             </ul>
-          </li>
-          <li>
-            <Link href='/upcoming' className='navbar-link'>
-              Upcoming
-            </Link>
-          </li>
-          <li>
-            <Link href='/favorite' className='navbar-link'>
-              Favorite
-            </Link>
-          </li>
-          <li>
-            <Link href='/watchlist' className='navbar-link'>
-              Watchlist
-            </Link>
-          </li>
+          </ul>
+          <NavbarLink href='/upcoming'>Upcoming</NavbarLink>
+          <NavbarLink href='/favorite'>Favorite</NavbarLink>
+          <NavbarLink href='/watchlist'>Watchlist</NavbarLink>
           <li>
             <form action={deleteSession}>
               <button className='text-xl button' type='submit'>
@@ -73,5 +42,19 @@ export default async function Header({
         </ul>
       </nav>
     </header>
+  );
+}
+
+type NavbarLinkProps = React.PropsWithChildren & {
+  href: string;
+};
+
+function NavbarLink({ children, href }: NavbarLinkProps) {
+  return (
+    <li>
+      <Link href={href} className='navbar-link whitespace-nowrap w-full'>
+        {children}
+      </Link>
+    </li>
   );
 }
