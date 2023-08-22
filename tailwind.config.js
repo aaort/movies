a6380c9189d90bb3bc1cd7f1669f70affaaa0258
aort <artashesano@gmail.com>
2023-08-22 12:36:41 +0400

    Set important to true in the tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const colors = require('tailwindcss/colors');
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
+  important: true,
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
