5d343c07e09dd52ea6c958c55812bff98f953102
aort <artashesano@gmail.com>
2023-08-27 11:24:51 +0400

    Include pagination on upcoming page

--- app/(authenticated)/(with_layout)/upcoming/page.tsx
+++ app/(authenticated)/(with_layout)/upcoming/page.tsx
@@ -1,9 +1,23 @@
 import GridList from '@/app/_components/GridList';
+import Pagination from '@/app/_components/Pagination';
 import MovieCard from '@/app/_components/cards/MovieCard';
 import get from '@/lib/api/get';
 
-export default async function UpcomingMovies() {
-  const movies = (await get<ResultType<Movie>>('/movie/upcoming'))?.results;
+type Props = {
+  searchParams: { [key: string]: string | null | undefined };
+};
+
+export default async function UpcomingMovies({ searchParams }: Props) {
+  const page = searchParams.page ?? 1;
+  const parsedPage = Number(page);
+
+  const url = `movie/upcoming`;
+  const data = await get<ResultType<Movie>>(url, {}, true, {
+    page: !isNaN(parsedPage) ? page : 1,
+  });
+
+  const movies = data?.results;
+  const totalPages = data?.total_pages;
 
   if (!movies) {
     throw new Error(
@@ -12,10 +26,17 @@ export default async function UpcomingMovies() {
   }
 
   return (
-    <GridList>
-      {movies?.map((movie, index) => (
-        <MovieCard key={movie.id} movie={movie} index={index} />
-      ))}
-    </GridList>
+    <section className='flex flex-col gap-y-10'>
+      <GridList>
+        {movies?.map((movie, index) => (
+          <MovieCard key={movie.id} movie={movie} index={index} />
+        ))}
+      </GridList>
+      <div className='self-end'>
+        {!isNaN(parsedPage) && totalPages ? (
+          <Pagination page={parsedPage} totalPages={totalPages} />
+        ) : null}
+      </div>
+    </section>
   );
 }
