da3ac7c655dff224267993eb85f4d3fb045f6b43
aort <artashesano@gmail.com>
2023-11-08 12:53:39 +0400

    Show trending people in the general page as well

--- app/(authenticated)/(with_layout)/general/page.tsx
+++ app/(authenticated)/(with_layout)/general/page.tsx
@@ -1,4 +1,5 @@
 import MovieCard from '@/app/_components/cards/MovieCard';
+import PersonCard from '@/app/_components/cards/PersonCard';
 import TVCard from '@/app/_components/cards/TVCard';
 import get from '@/lib/api/get';
 import Link from 'next/link';
@@ -6,6 +7,7 @@ import Link from 'next/link';
 export default async function GeneralPage() {
   const movies = (await get<ResultType<Movie>>('trending/movie/week'))?.results;
   const tvs = (await get<ResultType<TV>>('trending/tv/week'))?.results;
+  const people = (await get<ResultType<Person>>('person/popular'))?.results;
 
   return (
     <div className='space-y-10'>
@@ -41,6 +43,25 @@ export default async function GeneralPage() {
               tv={tv}
               index={i}
               key={tv.id}
+              className='min-w-[50vw] md:min-w-[30vw] lg:min-w-[20vw]'
+            />
+          ))}
+        </ul>
+      </section>
+
+      <section>
+        <div className='flex justify-between'>
+          <h2 className='mb-8 text-xl'>Trending - People</h2>
+          <Link href='/trending/tvs' className='underline'>
+            View all
+          </Link>
+        </div>
+        <ul className='flex gap-4 md:gap-8 overflow-x-auto pb-8'>
+          {people?.map((person, i) => (
+            <PersonCard
+              person={person}
+              index={i}
+              key={person.id}
               className='min-w-[50vw] md:min-w-[30vw] lg:min-w-[20vw]'
             />
           ))}
