446092cedce7d01fb353dc2428a83fa1e25f9f8a
aort <artashesano@gmail.com>
2023-11-09 21:17:49 +0400

    Make gap consistent in GridList

--- app/_components/GridList.tsx
+++ app/_components/GridList.tsx
@@ -2,7 +2,7 @@ type Props = React.PropsWithChildren & {};
 
 export default function GridList({ children }: Props) {
   return (
-    <ul className='grid gap-4 md:gap-8 xl:gap-10 grid-cols-2 md:grid-cols-3 xl:grid-cols-4'>
+    <ul className='grid gap-12 grid-cols-2 md:grid-cols-3 xl:grid-cols-4'>
       {children}
     </ul>
   );
