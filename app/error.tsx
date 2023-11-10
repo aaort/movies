3ea0692603e87247d907c4aca8e00a1bf4f2b122
aort <artashesano@gmail.com>
2023-11-10 12:31:41 +0400

    Remove extra code in app/error

--- app/error.tsx
+++ app/error.tsx
@@ -7,8 +7,8 @@ type Props = {
 
 export default function Error({ error, reset }: Props) {
   return (
-    <div className='flex flex-col items-center gap-4  absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-primary-800'>
-      <h2 className=''>{error.message}</h2>
+    <div className='flex flex-col items-center gap-4 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-primary-800'>
+      <h2>{error.message}</h2>
       <button onClick={reset} className='button'>
         Try Again
       </button>
