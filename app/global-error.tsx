a9ef91593fb1d72982c159ccb1957ba36e450f63
aort <artashesano@gmail.com>
2023-11-10 12:39:07 +0400

    Wrap global-error with html and body tags

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,5 +1,12 @@
 'use client';
 
+import { Lato } from 'next/font/google';
+
+const lato = Lato({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
+
 type Props = {
   error: Error;
   reset: () => void;
@@ -7,11 +14,15 @@ type Props = {
 
 export default function GlobalError({ reset }: Props) {
   return (
-    <div className='flex flex-col items-center gap-4  absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-primary-800'>
-      <h2>Sorry, something went wrong</h2>
-      <button onClick={reset} className='button'>
-        Try Again
-      </button>
-    </div>
+    <html lang='en'>
+      <body className={lato.className}>
+        <div className='flex flex-col items-center gap-4  absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-primary-800'>
+          <h2>Sorry, something went wrong</h2>
+          <button onClick={reset} className='button'>
+            Try Again
+          </button>
+        </div>
+      </body>
+    </html>
   );
 }
