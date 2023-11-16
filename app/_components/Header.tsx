2d40a864ea907aed717da86f5f67860b4385ed00
aort <artashesano@gmail.com>
2023-11-16 23:39:38 +0400

    Add whitespaces in Header

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -11,12 +11,14 @@ export default async function Header({ leftComponent }: Props) {
   return (
     <header className='flex justify-between gap-4 items-center horizontal-p py-8 bg-primary-100'>
       {leftComponent}
+
       <nav>
         <div className='block lg:hidden'>
           <Drawer>
             <Navbar className='relative w-[30%] min-w-[200px] right-0 h-full bg-primary-100 flex flex-col gap-8 z-[100] px-10 py-4 items-start' />
           </Drawer>
         </div>
+
         <Navbar className={classes} />
       </nav>
     </header>
