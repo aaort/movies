d03669679ecef7b7d992b66a2ee8a4ac2d135e52
aort <artashesano@gmail.com>
2023-11-16 23:23:34 +0400

    Add tmdb home page and api docs links on constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,6 @@
 // Maximum page for trending movies, tvs and people
-export const MAX_PAGE = 500;
+const MAX_PAGE = 500;
+const TMDB_LINK = 'https://www.themoviedb.org';
+const TMDB_API_DOCS_LINK = 'https://developer.themoviedb.org/docs';
+
+export { MAX_PAGE, TMDB_API_DOCS_LINK, TMDB_LINK };
